f53414a46d51001e5d22d4585064877082d85b64
tcarisland <thor.arisland@aurorainnovation.com>
2020-05-18 22:37:39 +0200

    stuff

--- tc-project-planner/gulpfile.js
+++ tc-project-planner/gulpfile.js
@@ -3,8 +3,22 @@ const sftp   = require('gulp-sftp');
 const fs     = require('file-system');
 const prompt = require('prompt');
 
+var login = require('./login.json');
+
+gulp.task('deploy', function() {
+  var globs = [ 'dist/*' ];
+  return gulp.src(globs, login).pipe(sftp({
+    host: login.host,
+    user: login.user,
+    port: login.port,
+    remotePath: login.remotePath,
+    privateKey: fs.readFileSync(login.privateKey),
+    timeout: login.timeout
+  }));
+});
+
 gulp.task('copy-php', function() {
     return gulp.src('src/*.php').pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', gulp.series(['copy-php']));
+gulp.task('default', gulp.series(['copy-php', 'deploy']));
