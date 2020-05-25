22974ee414fb33e2255f7feb166776faa78ad50f
tcarisland <thor.arisland@aurorainnovation.com>
2020-05-25 10:54:10 +0200

    cleaned up unused files

--- tc-project-planner/src/admin-list-projects.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include_once("tc-project-planner-utils.php");
-include_once("admin-list-project-renderer.php");
-echo list_and_render_projects();
- ?>
--- tc-project-planner/src/admin-query.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $project_name = $_GET["project_name"];
-  echo strrev($project_name);
-?>
