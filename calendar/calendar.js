43b1769eb8230611507b6169a092e7ff7900ecce
tcarisland <thor.arisland@aurorainnovation.com>
2020-05-31 17:21:24 +0200

    hello

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -8,24 +8,44 @@ $(window).resize(function() {
 
 var calendar = (function () {
   var iter = 0;
-  var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  var transp = 0.2;
+  var days = {
+    "Monday" : {
+      bgcolor : "rgba(0, 0, 0, " + transp + ")"
+    },
+    "Tuesday" : {
+      bgcolor : "rgba(255, 0, 0, " + transp + ")"
+    },
+    "Wednesday" : {
+      bgcolor : "rgba(0, 255, 0, " + transp + ")"
+    },
+    "Thursday" : {
+      bgcolor : "rgba(0, 0, 255, " + transp + ")"
+    },
+    "Friday" : {
+      bgcolor : "rgba(255, 255, 0, " + transp + ")"
+    },
+    "Saturday" : {
+      bgcolor : "rgba(0, 255, 255, " + transp + ")"
+    },
+    "Sunday" : {
+      bgcolor : "rgba(255, 0, 255, " + transp + ")"
+    }
+  };
   
   function renderCalendar() {
     var templateColumns = "";
-    for(var i = 0; i < days.length; i++) {
-      templateColumns += "" + ($("#calendarWrapper").width() / days.length) + "px ";
+    for(var i = 0; i < Object.keys(days).length; i++) {
+      templateColumns += "" + ($("#calendarWrapper").width() / Object.keys(days).length) + "px ";
     }
     $("#calendarWrapper").html("");
     $("#calendarWrapper").css("grid-template-columns", templateColumns);
-    for(var i = 0; i < days.length; i++) {
-      var r = Math.floor(Math.random() * 255);
-      var g = Math.floor(Math.random() * 255);
-      var b = Math.floor(Math.random() * 255);
-      var color = "rgba(" + r + ", " + g + ", " + b + ", 0.2)";
-      $("#calendarWrapper").append("<div id='calendar" + days[i] + "' style='text-align: center; background-color: " + color + "'></div>");
-      $("#calendar" + days[i]).append(days[i]);
-      $("#calendar" + days[i]).append("<br>");
-      $("#calendar" + days[i]).append("<span style='display: block; width: 100%; height: 100%; margin-top: 40px; background-color: rgba(255,255,255,0.5)'></span>");
+    for(var day in days) {
+      var color = days[day].bgcolor;
+      var border = day != "Sunday" ? "3px solid #FFF" : "none";
+      var style = "text-align: center; background-color: " + color + "; border-right: " + border + ";";
+      $("#calendarWrapper").append("<div id='calendar" + day + "' style='" + style + "'></div>");
+      $("#calendar" + day).append("<div class='calendarDayHeader'>" + day + "</div>");
     }
   }
   
