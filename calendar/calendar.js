843b4934e8d7f6e3c08349a6038ed0e2e5114f49
tcarisland <thor.arisland@aurorainnovation.com>
2020-06-02 15:39:38 +0200

    more schedule fixes

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -11,6 +11,9 @@ $(document).ready(function() {
     "dumbday" : ""
   }
 	calendar.init(days);
+  calendar.add(["08:00", "12:00"], "wednesday");
+  calendar.add(["11:30", "14:45"], "wednesday");
+  calendar.add(["13:00", "17:00"], "wednesday");
 });
 
 var calendar = ( function() {
@@ -26,6 +29,24 @@ var calendar = ( function() {
   
   function intToHour(h) {
     return ("0" + h).substr(("0" + h).length - 2) + ":00";
+  }
+  
+  var addInterval = function(interval, day) {
+    //grid-row: 1;  margin-top: 1320px; height: 30px;
+    var from = timeToInt(interval[0]);
+    var to = timeToInt(interval[1]);
+    var intervalID = "interval_" + from + "_" + to;
+    var intervalText = interval[0] + " - " + interval[1]
+    var intervalElement = "<div id='" + intervalText +"' title='" + intervalText + "' class='openingHours' style='grid-row: 1; margin-top: " + from + "px; height: " + (to - from) + "px; grid-column: " + day + ";'>";
+    intervalElement += "<div class='openingHoursText'>" + intervalText + "</div>";
+    intervalElement += "</div>";
+    console.log(intervalElement);
+    $("#schedule").append(intervalElement);
+  }
+  
+  function timeToInt(time) {
+    var hoursAndMinutes = time.split(":");
+    return parseInt(hoursAndMinutes[0]) * 60 + parseInt(hoursAndMinutes[1]);
   }
 
 	var initialize = function (days) {
@@ -55,6 +76,7 @@ var calendar = ( function() {
 	}
 
 	return {
-  	init : initialize
+  	init : initialize,
+    add : addInterval
   }
 }());
