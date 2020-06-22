5f2bc06bd30ba698702da9eec22012795c0e1d49
tcarisland <thor.arisland@aurorainnovation.com>
2020-06-22 23:13:09 +0200

    react application works

--- electron_react/webpack.config.js
+++ electron_react/webpack.config.js
@@ -5,6 +5,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = [
     {
 	mode: 'development',
+	entry: './src/electron.ts',
+	target: 'electron-main',
+	module: {
+	    rules: [{
+		test: /\.ts$/,
+		include: /src/,
+		use: [{ loader: 'ts-loader' }]
+	    }]
+	},
+	output: {
+	    path: __dirname + '/dist',
+	    filename: 'electron.js'
+	}
+    },
+    {
+	mode: 'development',
 	entry: './src/react.tsx',
 	target: 'electron-renderer',
 	devtool: 'source-map',
@@ -15,7 +31,7 @@ module.exports = [
 	    }] },
 	output: {
 	    path: __dirname + '/dist',
-	    filename: 'electron.js'
+	    filename: 'react.js'
 	},
 	plugins: [
 	    new HtmlWebpackPlugin({
